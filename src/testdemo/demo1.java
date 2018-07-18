52faa14747ee2ef8f19fd2a62642b25742d6f298
cthitting <pengwen.135@163.com>
2018-07-18 09:38:50 +0800

    try a testdemo on the master

--- src/testdemo/demo1.java
+++ src/testdemo/demo1.java
@@ -4,4 +4,12 @@ public class demo1 {
 	public int age;
 	public String name;
 	public String sex;
+
+	demo1(int age,String name,String sex){
+		this.age = age;
+		this.name = name;
+		this.sex = sex;
+	}
+
+
 }
