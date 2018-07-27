ee55971990eeb107741990014766abcf1b2e45d2
cthitting <943422343@qq.com>
2018-07-27 16:01:09 +0800

    demo2 commit to test merge reset

--- src/testdemo/demo2.java
+++ src/testdemo/demo2.java
@@ -4,9 +4,20 @@ public class demo2 {
 	public String name;
 	public int age;
 	public String sex;
+	public int count;
 
 	demo2(){
 		System.out.println("first status");
 		System.out.println("test a rebase - testing");
+	}
+
+	public void Calculate(int x,int y){
+		count = x*y;
+	}
+
+
+
+	public static void main(String[] args){
+		demo2 d2 = new demo2();
 	}
 }
